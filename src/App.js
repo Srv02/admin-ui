a04e0c5df18669bfece131dbca3454f75e073412
Saurav Thapa <thapasaurav1996@gmail.com>
2022-10-21 21:42:11 +0530

    removed console log

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-
-import {useState, useEffect} from 'react';
-import './App.css';
-import TableComponent from './components/Table/TableComponent';
+import { useState, useEffect } from "react";
+import "./App.css";
+import TableComponent from "./components/Table/TableComponent";
 
 function App() {
-  const[data, setData]=useState([]);
-  useEffect (()=>{
-    const url = 'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
-    fetch (url).then(response => response.json())
-    .then((data)=>{
-      console.log('DATA ',data);
-      setData(data);
-    }); 
-  },[]);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const url =
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
   console.log(data.length);
   return (
     <div className="App">
-      {data.length ? <TableComponent data={data}/> : <div className="loader"></div>}
+      {data.length ? (
+        <TableComponent data={data} />
+      ) : (
+        <div className="loader"></div>
+      )}
     </div>
   );
 }
